6c3e29cc33946bbb47a58b4653672b2c2466c2d7
daniel <joenah004@gmail.com>
2025-05-28 18:47:43 +0100

    footer_fix

--- frontend/src/app/api/auth/google/route.ts
+++ frontend/src/app/api/auth/google/route.ts
@@ -1,7 +1,3 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const googleAuthUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/google`;
-
-  return NextResponse.redirect(googleAuthUrl);
-}
+export async function GET() {}
